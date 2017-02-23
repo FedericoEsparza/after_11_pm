fec54fc062653a4d267be0020370a9d98d91706a
Joe Zhou <chn.challenger@gmail.com>
2017-02-23 18:22:15 +0000

    more cases working

--- helpers/objectify.rb
+++ helpers/objectify.rb
@@ -1,13 +1,22 @@
 module Objectify
   def objectify(str)
+    #2+x+y
     if str.include?('+')
       str_args = str.split('+')
       object_args = str_args.inject([]){ |r,e| r << objectify(e) }
       return add(object_args)
     end
+    #2xyz
+    if str.length > 1 && !(/[^A-Za-z0-9_]/ =~ str)
+      str_args = str.split('')
+      object_args = str_args.inject([]){ |r,e| r << objectify(e) }
+      return mtp(object_args)
+    end
+    #string variable
     if str.length == 1 && /[A-Za-z]/ =~ str
       return str
     end
+    #number
     if /[0-9]/ =~ str
       return str.to_i
     end
--- spec/helpers/objectify_spec.rb
+++ spec/helpers/objectify_spec.rb
@@ -4,8 +4,24 @@ describe Objectify do
   let(:dummy_class){(Class.new{include Objectify}).new}
 
   describe '#objectify' do
-    it '' do
+    it 'x+2' do
       expect(dummy_class.objectify('x+2')).to eq add('x',2)
+    end
+
+    it 'x+y+2' do
+      expect(dummy_class.objectify('x+y+2')).to eq add('x','y',2)
+    end
+
+    it '2x' do
+      expect(dummy_class.objectify('2x')).to eq mtp(2,'x')
+    end
+
+    it '3xyz' do
+      expect(dummy_class.objectify('3xyz')).to eq mtp(3,'x','y','z')
+    end
+
+    it '3x+5' do
+      expect(dummy_class.objectify('3x+5')).to eq add(mtp(3,'x'),5)
     end
   end
 
