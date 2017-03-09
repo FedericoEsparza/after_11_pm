78ed1084d47dbfcd70f3e63334bc12f7fc5fcf0c
Joe Zhou <chn.challenger@gmail.com>
2017-03-09 13:45:04 +0000

    first trig angle obtained recursively

--- helpers/trig_utilities.rb
+++ helpers/trig_utilities.rb
@@ -1,17 +1,53 @@
 module TrigUtilities
   def same_angles?(exp)
-    result = true
-    current_var = nil
+    angle = first_trig_angle(exp)
+    all_trig_this_angle?(exp,angle)
+    # result = true
+    # current_var = nil
+    # exp.args.each do |a|
+    #   if (numerical?(a) || a.is_a?(string)) == false
+    #     if a.is_a?(sine) || a.is_a?(cosine)
+    #       if current_var == nil
+    #         current_var = a.angle
+    #       else
+    #         if current_var != a.angle
+    #           return false
+    #         end
+    #       end
+    #     else #not string num or trig
+    #
+    #
+    #     end
+    #   end
+    # end
+    # return true
+  end
+
+  def first_trig_angle(exp)
     exp.args.each do |a|
       if (numerical?(a) || a.is_a?(string)) == false
         if a.is_a?(sine) || a.is_a?(cosine)
-          if current_var == nil
-            current_var = a.angle
-          else
-            if current_var != a.angle
-              return false
-            end
+          return a.angle
+        else
+          angle = first_trig_angle(a)
+          return angle unless angle.nil?
+        end
+      end
+    end
+    return nil
+  end
+
+  def all_trig_this_angle?(exp,angle)
+    current_var = angle
+    exp.args.each do |a|
+      if (numerical?(a) || a.is_a?(string)) == false
+        if a.is_a?(sine) || a.is_a?(cosine)
+          if current_var != a.angle
+            return false
           end
+        else #not string num or trig
+
+
         end
       end
     end
--- spec/helpers/trig_utilities_spec.rb
+++ spec/helpers/trig_utilities_spec.rb
@@ -3,11 +3,46 @@ require './helpers/trig_utilities'
 describe TrigUtilities do
   let(:dummy_class){(Class.new{include TrigUtilities}).new}
 
-  describe '#same_angles?' do
-    it '3+sin x+2cos x returns true' do
-      exp = '3+\sin y+2\cos y'.objectify
-      expect(dummy_class.same_angles?(exp)).to eq true
+  describe '#first_trig_angle' do
+    it 'find the first trig angle for 3+\sin y+\cos y' do
+      exp = '3+\sin y+\cos y'.objectify
+      expect(dummy_class.first_trig_angle(exp)).to eq 'y'
+    end
+
+    it 'find the first trig angle for 3+2\sin y' do
+      exp = '3+2\sin y'.objectify
+      expect(dummy_class.first_trig_angle(exp)).to eq 'y'
+    end
+
+    it 'recursively find the first trig angle for 5-\frac{3+2\sin y}{2+x}' do
+      exp = '5-\frac{3+2\sin y}{2+x}'.objectify
+      expect(dummy_class.first_trig_angle(exp)).to eq 'y'
+    end
+
+    it 'returns nil for 5-\frac{3+2y}{2+x}' do
+      exp = '5-\frac{3+2y}{2+x}'.objectify
+      expect(dummy_class.first_trig_angle(exp)).to eq nil
     end
   end
+
+  describe '#same_angles?' do
+    it '3+sin x+ cos x returns true' do
+      exp = '3+\sin y+\cos y'.objectify
+      expect(dummy_class.same_angles?(exp)).to eq true
+    end
+
+    it '3+sin y+cos x returns false' do
+      exp = '3+\sin y+\cos x'.objectify
+      expect(dummy_class.same_angles?(exp)).to eq false
+    end
+
+    xit '3+sin y+cos x returns false' do
+      exp = '3+\sin y+2\cos x'.objectify
+      expect(dummy_class.same_angles?(exp)).to eq false
+    end
+  end
+
+
+
 
 end
