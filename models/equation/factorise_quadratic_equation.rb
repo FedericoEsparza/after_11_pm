15054dc946dc59757aadcdbc915a739ad0a71394
Joe Zhou <chn.challenger@gmail.com>
2017-03-10 17:44:57 +0000

    refactored quadratic infor

--- models/equation/factorise_quadratic_equation.rb
+++ models/equation/factorise_quadratic_equation.rb
@@ -1,123 +1,38 @@
 module FactoriseQuadraticEquation
-  def is_quadratic?
+  def _quadratic_infor
     eqn_copy = self.copy
 
-    left_side_args = []
-    if ls.is_a?(addition)
-      left_side_args = copy.ls.args
-    else
-      left_side_args = [copy.ls]
-    end
+    lft_side_args = ls.is_a?(addition) ? copy.ls.args : [copy.ls]
+    rgt_side_args = rs.is_a?(addition) ? copy.rs.args : [copy.rs]
 
-    right_side_args = []
-    if rs.is_a?(addition)
-      right_side_args = copy.rs.args
-    else
-      right_side_args = [copy.rs]
-    end
+    all_args = lft_side_args + rgt_side_args
 
-    all_args = left_side_args + right_side_args
-
-    all_args.each do |arg|
+    uniques = all_args.inject([]) do |res,arg|
       if arg.is_a?(multiplication)
-        new_args = arg.args.select do |a|
-          numerical?(a) == false
-        end
-        arg.args = new_args
+        arg.args = arg.args.select { |a| numerical?(a) == false }
       end
+      if !numerical?(arg.flatten) && !res.include?(arg.flatten)
+        res << arg.flatten
+      end
+      res
     end
 
-    all_args = all_args.map do |arg|
-      arg.flatten
-    end
-
-    variable_args = all_args.select do |arg|
-      numerical?(arg) == false
-    end
-
-    uniques = []
-
-    variable_args.each do |var|
-      uniques << var unless uniques.include?(var)
-    end
-
-    return false unless uniques.length == 2
+    return {is_quadratic?:false,var:nil} unless uniques.length == 2
 
     check_1 = div(uniques[0].copy,uniques[1].copy).simplify
-
-    if check_1.~(uniques[1])
-      return true
-    end
+    return {is_quadratic?:true,var:uniques[1]} if check_1.~(uniques[1])
 
     check_2 = div(uniques[1].copy,uniques[0].copy).simplify
+    return {is_quadratic?:true,var:uniques[0]} if check_2.~(uniques[0])
 
-    if check_2.~(uniques[0])
-      return true
-    end
+    return {is_quadratic?:false,var:nil}
+  end
 
-    return false
-
+  def is_quadratic?
+    _quadratic_infor[:is_quadratic?]
   end
 
   def quadratic_var
-    eqn_copy = self.copy
-
-    left_side_args = []
-    if ls.is_a?(addition)
-      left_side_args = copy.ls.args
-    else
-      left_side_args = [copy.ls]
-    end
-
-    right_side_args = []
-    if rs.is_a?(addition)
-      right_side_args = copy.rs.args
-    else
-      right_side_args = [copy.rs]
-    end
-
-    all_args = left_side_args + right_side_args
-
-    all_args.each do |arg|
-      if arg.is_a?(multiplication)
-        new_args = arg.args.select do |a|
-          numerical?(a) == false
-        end
-        arg.args = new_args
-      end
-    end
-
-    all_args = all_args.map do |arg|
-      arg.flatten
-    end
-
-    variable_args = all_args.select do |arg|
-      numerical?(arg) == false
-    end
-
-    uniques = []
-
-    variable_args.each do |var|
-      uniques << var unless uniques.include?(var)
-    end
-
-    return false unless uniques.length == 2
-
-    check_1 = div(uniques[0].copy,uniques[1].copy).simplify
-
-    if check_1.~(uniques[1])
-      return uniques[1]
-    end
-
-    check_2 = div(uniques[1].copy,uniques[0].copy).simplify
-
-    if check_2.~(uniques[0])
-      return uniques[0]
-    end
-
-    return nil
+    _quadratic_infor[:var]
   end
-
-
-
 end
