b3023ce94d021834ee1301f906be9e831c513c4d
Joe Zhou <chn.challenger@gmail.com>
2017-03-10 12:19:04 +0000

    WIP is quadratic

--- models/equation/factorise_quadratic_equation.rb
+++ models/equation/factorise_quadratic_equation.rb
@@ -1,9 +1,46 @@
 module FactoriseQuadraticEquation
+  def is_quadratic?
+    eqn_copy = self.copy
 
-  def test
-    ls
+    left_side_args = []
+    if ls.is_a?(addition)
+      left_side_args = copy.ls.args
+    else
+      left_side_args = [copy.ls]
+    end
+
+    right_side_args = []
+    if rs.is_a?(addition)
+      right_side_args = copy.rs.args
+    else
+      right_side_args = [copy.rs]
+    end
+
+    all_args = left_side_args + right_side_args
+
+    all_args.each do |arg|
+      if arg.is_a?(multiplication)
+        new_args = arg.args.select do |a|
+          numerical?(a) == false
+        end
+        arg.args = new_args
+      end
+    end
+
+    all_args = all_args.map do |arg|
+      flatten(arg)
+    end
+
+    variable_args = all_args.select do |arg|
+      numerical?(arg) == false
+    end
+
+    variable_args
+
+      
+
   end
 
-  
+
 
 end
--- spec/equation/factorise_quadratic_equation_spec.rb
+++ spec/equation/factorise_quadratic_equation_spec.rb
@@ -1,9 +1,12 @@
 describe FactoriseQuadraticEquation do
-  describe '#test' do
-    it 'matches outer brackets in \frac{frac{2}{y}}{x}' do
-      equation = eqn('x',1)
-      puts equation.test
+  describe '#is_quadratic?' do
+    it 'returns true for 0=4x^2+3x-5' do
+      equation = '0=4x^2+3x-5'.objectify
+      expect(equation.is_quadratic?).to eq [pow('x',2),'x']
+      expect(equation).to eq '0=4x^2+3x-5'.objectify
     end
   end
+
+
 
 end
